95f67a3e171afc5a18848d1cca58e8969de8f2e5
Krystal Schenk <kschenk@uta.edu>
2018-08-15 14:14:48 -0500

    theming

--- themes/makerlit_sass/templates/page.tpl.php
+++ themes/makerlit_sass/templates/page.tpl.php
@@ -81,7 +81,7 @@
       <?php endif; ?>
     <div class="navbar-header">
       <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+        <a class="logo navbar-btn" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
