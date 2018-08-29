54634fbdd525987190556490ef1b18a125a24bf6
Krystal Schenk <kschenk@uta.edu>
2018-08-29 11:51:08 -0500

    brand/logo

--- themes/makerlit_sass/templates/html.tpl.php
+++ themes/makerlit_sass/templates/html.tpl.php
@@ -57,7 +57,7 @@
 <html<?php print $html_attributes;?><?php print $rdf_namespaces;?>>
 <head>
   <link rel="profile" href="<?php print $grddl_profile; ?>" />
-  <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600|Open+Sans" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600|Open+Sans|Playfair+Display" rel="stylesheet"> 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
