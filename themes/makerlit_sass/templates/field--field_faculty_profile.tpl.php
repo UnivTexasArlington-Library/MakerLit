8e960560f73ca1ac9c486a4fd132e32922622ab3
Krystal Schenk <kschenk@uta.edu>
2018-09-20 15:57:52 -0500

    template & stuff

--- themes/makerlit_sass/templates/field--field_faculty_profile.tpl.php
+++ themes/makerlit_sass/templates/field--field_faculty_profile.tpl.php
@@ -59,7 +59,7 @@ if (!$label_hidden) { ?>
 } ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item) { ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even';   ?>"<?php print $item_attributes[$delta];   ?>>"<?php print render($item);   ?>"<i class="glyphicon glyphicon-new-window"></i></div>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even';   ?><?php print $item_attributes[$delta];   ?>>"<?php print render($item);   ?><i class="glyphicon glyphicon-new-window"></i></div>
     <?php } ?>
   </div>
 </div>
