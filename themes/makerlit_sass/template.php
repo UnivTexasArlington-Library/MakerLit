3d8880a6b951ef307141b664ee3850dbb539d8b6
Krystal Schenk <kschenk@uta.edu>
2018-09-26 10:10:15 -0500

    again

--- themes/makerlit_sass/template.php
+++ themes/makerlit_sass/template.php
@@ -5,7 +5,7 @@
  * The primary PHP file for this theme.
  */
 function makerlit_sass_preprocess_block (&$vars) {
-	 if ($vars['block']->bid === 'pane_block_8') {
+	 if ($vars['block']->bid === 'block_block_8') {
 	 	$title_classes = &$vars['title_attributes_array']['inline'];
 	 	$content_classes = &$vars['content_attributes_array']['inline inline-link'];
   }	 
