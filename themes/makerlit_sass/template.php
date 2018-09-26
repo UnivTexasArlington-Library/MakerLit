799f41edfae89cd1252b21a4126a9e6f53cbf58c
Krystal Schenk <kschenk@uta.edu>
2018-09-26 10:08:26 -0500

    template test

--- themes/makerlit_sass/template.php
+++ themes/makerlit_sass/template.php
@@ -4,3 +4,9 @@
  * @file
  * The primary PHP file for this theme.
  */
+function makerlit_sass_preprocess_block (&$vars) {
+	 if ($vars['block']->bid === 'pane_block_8') {
+	 	$title_classes = &$vars['title_attributes_array']['inline'];
+	 	$content_classes = &$vars['content_attributes_array']['inline inline-link'];
+  }	 
+}
