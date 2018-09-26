9aa218989c20edd0e7fc89a54616f9dbc2451517
Krystal Schenk <kschenk@uta.edu>
2018-09-26 10:12:11 -0500

    template again

--- themes/makerlit_sass/template.php
+++ themes/makerlit_sass/template.php
@@ -4,9 +4,12 @@
  * @file
  * The primary PHP file for this theme.
  */
-function makerlit_sass_preprocess_block (&$vars) {
-	 if ($vars['block']->bid === 'block_block_8') {
+function makerlit_sass_preprocess_block__block_block_8 (&$vars) {
 	 	$title_classes = &$vars['title_attributes_array']['inline'];
 	 	$content_classes = &$vars['content_attributes_array']['inline inline-link'];
-  }	 
+}
+
+function makerlit_sass_preprocess_block__block_8 (&$vars) {
+	 	$title_classes = &$vars['title_attributes_array']['inline'];
+	 	$content_classes = &$vars['content_attributes_array']['small inline inline-link'];
 }
