d117146ff4700ba74832931217831f6d0839302b
Krystal Schenk <kschenk@uta.edu>
2018-09-26 10:15:37 -0500

    pane titles on homepage

--- themes/makerlit_sass/template.php
+++ themes/makerlit_sass/template.php
@@ -4,12 +4,3 @@
  * @file
  * The primary PHP file for this theme.
  */
-function makerlit_sass_preprocess_block__block_block_8 (&$vars) {
-	 	$title_classes = &$vars['title_attributes_array']['inline'];
-	 	$content_classes = &$vars['content_attributes_array']['inline inline-link'];
-}
-
-function makerlit_sass_preprocess_block__block_8 (&$vars) {
-	 	$title_classes = &$vars['title_attributes_array']['inline'];
-	 	$content_classes = &$vars['content_attributes_array']['small inline inline-link'];
-}
